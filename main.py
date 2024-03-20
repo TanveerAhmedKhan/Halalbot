2fa545954b1af10f62ccd42e593d23fcdee2440a
Mohsin_Senarios <mohsinali.senarios@gmail.com>
2024-03-20 13:54:26 +0500

    mohsin_ init

--- main.py
+++ main.py
@@ -40,3 +40,8 @@ def root():
 
 
 app.include_router(apis, prefix="/api")
+
+# Run the FastAPI application
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=5000)
